ff07cdcedadd59fe545a28dfb1ba1a05a088c6ad
MiguelSant7 <miguelsantoss006@gmail.com>
2025-02-08 23:37:54 -0300

    Primeira versão oficial

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.querySelectorAll('.link-zap').forEach(button => {
     button.addEventListener('click', function() {
         // Cria um link temporário
         const a = document.createElement('a');
-        a.href = 'https://api.whatsapp.com/send?phone=553193823683&text=Seja%20bem%20vindo%20ao%20Sertec%20Contabilidade%20e%20consultoria.%20%C3%89%20um%20prazer%20falar%20com%20voc%C3%AA!%20%F0%9F%93%88%0A%0ADigite%20seu%20nome%20e%20a%20cidade%20de%20onde%20est%C3%A1%20falando%20que%20j%C3%A1%20vamos%20te%20atender.%20%F0%9F%A4%9D'; // Substitua com o URL desejado
+        a.href = 'https://api.whatsapp.com/send?phone=5531999867858&text=Seja%20bem%20vindo%20ao%20Tiger Accounting%20Contabilidade%20e%20consultoria.%20%C3%89%20um%20prazer%20falar%20com%20voc%C3%AA!%20%F0%9F%93%88%0A%0ADigite%20seu%20nome%20e%20a%20cidade%20de%20onde%20est%C3%A1%20falando%20que%20j%C3%A1%20vamos%20te%20atender.%20%F0%9F%A4%9D'; // Substitua com o URL desejado
         a.target = '_blank'; // Abre o link em uma nova aba
         a.rel = 'noopener noreferrer'; // Adiciona segurança
         // Simula um clique no link
